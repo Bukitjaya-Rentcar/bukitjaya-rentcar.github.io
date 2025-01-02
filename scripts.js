64d4bedf65293bc7f0672ecaef021ec915af3422
Bukitjaya-Rentcar <bukitjayapcrentcar@gmail.com>
2025-01-02 16:08:35 +0800

    update
    uploade

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function toggleLanguage(lang) {
+    const elements = document.querySelectorAll('[data-lang]');
+    elements.forEach(el => {
+        if (el.getAttribute('data-lang') === lang) {
+            el.classList.remove('hidden');
+        } else {
+            el.classList.add('hidden');
+        }
+    });
+}
